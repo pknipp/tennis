b3dab78c1b4aaad07c74eeef1cdbc47ae391d510
Peter Knipp <peteraknipp@gmail.com>
2021-04-03 06:49:47 -0400

    git push

--- client/src/components/Member.js
+++ client/src/components/Member.js
@@ -1,10 +1,11 @@
 import React from "react";
 import cartoon from "./cartoon.png";
+const CARTOON = `https://tennis-photos.s3.us-east-2.amazonaws.com/uploads/SatApr30643252021.png`
 
 const Member = ({user}) => (
     <tr>
         <td>{user.name}</td><td>{user.email}</td><td>{user.phone}</td>
-        <td><img className="small" src={user.photo_url || cartoon} /></td>
+        <td><img className="small" src={user.photo_url || CARTOON} /></td>
     </tr>
 )
 
