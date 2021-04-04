fec2730c7e37aa468af977d53eb68f7fccab57f0
Peter Knipp <peteraknipp@gmail.com>
2021-04-03 21:59:04 -0400

    Start modifying the way that photos are rendered.

--- client/src/components/Member.js
+++ client/src/components/Member.js
@@ -1,16 +1,18 @@
-import React from "react";
-// import cartoon from "./cartoon.png";
+import React, {useContext} from "react";
+import AuthContext from '../auth';
 const CARTOON = `https://tennis-photos.s3.us-east-2.amazonaws.com/uploads/SatApr30643252021.png`
 
-const Member = ({user}) => (
+const Member = ({user}) => {
+    const { currentUser } = useContext(AuthContext)
+    return (
     <tr>
         <td>{user.name}</td><td>{user.email}</td><td>{user.phone}</td>
         <td>
-            <img className="small" src={user.photo_url || CARTOON}
-                alt={user.photo_url ? user.name : "a cartoon character"}
-            />
+            {(!user.photo_url && currentUser.id === user.id) ? "Submit photo" :
+                <img className="small" src={user.photo_url || CARTOON} />
+            }
         </td>
     </tr>
-)
+)}
 
 export default Member;
